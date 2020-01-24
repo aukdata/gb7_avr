b91155e098b9b4750dc26bd43cc694e64f20ed66
takuma <takuma.y0524@gmail.com>
2020-01-24 09:28:50 +0900

    Change: reduce queue size

--- firmware/src/speaker.hpp
+++ firmware/src/speaker.hpp
@@ -36,7 +36,7 @@ namespace gb7::sound
             uint32_t length; // microseconds
         };
 
-        static inline queue<Note, 128> m_notes;
+        static inline queue<Note, 16> m_notes;
         static inline Tone m_tone = Tone::None;
         static inline uint32_t m_count = 0;
         static inline uint32_t m_count_to = 0;
